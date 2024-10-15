b4f27c91f77dae1b5e7de9731eb4ffb44bd76066
Md Akash <you@example.com>
2024-10-15 20:51:05 +0600

    Edit Project Page

---
+++ app/Models/Project.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+    // protected $fillable=['name', 'nid', 'dob','email','phone', 'joiningDate', 'employeId', 'designation', 'language', 'department', 'signature', 'password', 'photo','country', 'districts','upozila','post','zipCode','state'];
+}
