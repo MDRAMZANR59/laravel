adb785bfe0c34269a5d3f53a62b0edd27df0b53d
Md Akash <you@example.com>
2024-10-31 21:28:10 +0600

    Edit Photo Option

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -14,8 +14,22 @@ class UserController extends BaseController
         return $this->sendResponse($data,"user Data");
     }
     public function store(Request $request){
+        /* for files */
+   $input=$request->all();
+   $files=[];
+   if($request->hasFile('files')){
+       foreach($request->file('files') as $f){
+           $photoname=time().rand(1111,9999).".".$f->extension();
+           $photoPath=public_path().'/adduser';
+           if($f->move($photoPath,$photoname)){
+               array_push($files,$photoname);
+           }
+       }
+   }
+   $input['photo']=implode(',',$files);
+   /* /for files */
 
-        $data=User::create($request->all());
+        $data=User::create($input);
         $input=$request->all();
         $input['user_id']=$data->id;
         $userdata=UserMeta::create($input);
@@ -25,7 +39,21 @@ class UserController extends BaseController
         return $this->sendResponse($User,"SecondaryUser created successfully");
     }
     public function update(Request $request,$id){
-        $data=User::where('id',$id)->update($request->all());
+        $input=$request->all();
+        /* for files */
+        $files=[];
+        if($request->hasFile('files')){
+            foreach($request->file('files') as $f){
+                $photoname=time().rand(1111,9999).".".$f->extension();
+                $photoPath=public_path().'/adduser';
+                if($f->move($photoPath,$photoname)){
+                    array_push($files,$photoname);
+                }
+            }
+            $input['photo']=implode(',',$files);
+        }
+        unset($input['files']);
+        $data=User::where('id',$id)->update($input);
         return $this->sendResponse($id,"SecondaryUser updated successfully");
     }
     
