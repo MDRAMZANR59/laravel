7b381b2e683b702820bb55c353596ac98cb5a583
Md Akash <you@example.com>
2024-11-01 13:01:36 +0600

    Add Costomer Note Attachment

--- app/Http/Controllers/Api/CustomerNoteController.php
+++ app/Http/Controllers/Api/CustomerNoteController.php
@@ -13,6 +13,23 @@ class CustomerNoteController extends BaseController
         return $this->sendResponse($data,"Customer Note Data");
     }
     public function store(Request $request){
+        /* for files */
+   $input=$request->all();
+   $files=[];
+   if($request->hasFile('files')){
+       foreach($request->file('files') as $f){
+           $photoname=time().rand(1111,9999).".".$f->extension();
+           $photoPath=public_path().'/customerNote';
+           if($f->move($photoPath,$photoname)){
+               array_push($files,$photoname);
+           }
+       }
+   }
+   $input['attachment']=implode(',',$files);
+        /* /for files */
+        $data=CustomerNote::create($input);
+        $input=$request->all();
+        $input['id']=$data->id;
         $data=CustomerNote::create($request->all());
         return $this->sendResponse($data,"Customer Note created successfully");
     }
@@ -20,6 +37,20 @@ class CustomerNoteController extends BaseController
         return $this->sendResponse($customerNote,"Customer Note created successfully");
     }
     public function update(Request $request,$id){
+        $input=$request->all();
+        /* for files */
+        $files=[];
+        if($request->hasFile('files')){
+            foreach($request->file('files') as $f){
+                $photoname=time().rand(1111,9999).".".$f->extension();
+                $photoPath=public_path().'/customerNote';
+                if($f->move($photoPath,$photoname)){
+                    array_push($files,$photoname);
+                }
+            }
+            $input['attachment']=implode(',',$files);
+        }
+        unset($input['files']);
         $data=CustomerNote::where('id',$id)->update($request->all());
         return $this->sendResponse($id,"Customer Note updated successfully");
     }
