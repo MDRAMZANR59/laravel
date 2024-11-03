e20398da5958e5ba36f9c457075c7255bcf04fbd
Md Akash <you@example.com>
2024-11-04 00:28:31 +0600

    edit React Model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,7 +10,7 @@ class Task extends Model
     use HasFactory;
     protected $fillable=['projectName', 'employeeId', 'note', 'task', 'assignDate', 'finishDate', 'actualDate'];
 
-    // relateion 
+    // relateion
     // public function projectfiles(){
     //     return $this->belongsTo(Projectfiles::class);
     // }
